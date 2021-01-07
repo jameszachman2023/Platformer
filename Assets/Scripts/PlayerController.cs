f8d3919a79ff67c6505260a6f35ce1c708afd03a
jameszachman2023 <james.d.zachman@gmail.com>
2021-01-07 13:37:26 -0600

    Added a "jump" and right direction movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpPower;
+    public float MovementPower;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * jumpPower);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+            {
+                rb.AddForce(transform.right * MovementPower);
+
+            }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            rb.AddForce(transform.left * MovementPower);
+
+        }
+
+    }
+}
